3be640a9daecd122d964bca12b19ec5078643616
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-16 15:23:42 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import '../assets/css/Home.css'
+import { Intro } from '../components/Intro'
+import { ProjectContainer } from '../components/ProjectContainer'
 
 export const Home = () => {
-	return <div className="Home">Home</div>
+	return (
+		<div className="Home">
+			<Intro />
+			<ProjectContainer />
+		</div>
+	)
 }
