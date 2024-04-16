32b2df61b18abbc9bf07484f07eda0ae7e45148f
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-16 20:01:36 +0200

    Created style
    Create style files for ProjectContainer & ProjectPreview

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,4 +1,4 @@
-//import '../assets/css/ProjectContainer.css'
+import '../assets/css/ProjectContainer.css'
 import projects from '../assets/projets.json'
 import { ProjectPreview } from './ProjectPreview'
 import { Sorter } from './Sorter'
@@ -6,6 +6,7 @@ import { Sorter } from './Sorter'
 export const ProjectContainer = () => {
 	return (
 		<div className="ProjectContainer">
+			<h2>MES PROJETS</h2>
 			<Sorter />
 			{projects.map((projet, index) => (
 				<ProjectPreview projet={projet} key={index} />
--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,4 +1,4 @@
-//import '../assets/css/ProjectPreview.css'
+import '../assets/css/ProjectPreview.css'
 
 export const ProjectPreview = ({ projet }) => {
 	return <div className="ProjectPreview">{projet.nom}</div>
