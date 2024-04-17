91693cfd7705f4cdcccee44c5189a76508b2fec5
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-17 16:42:34 +0200

    Image import logic

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,5 +1,10 @@
 import '../assets/css/ProjectPreview.css'
 
 export const ProjectPreview = ({ projet }) => {
-	return <div className="ProjectPreview">{projet.nom}</div>
+	const importedImage = require('../assets/images/' + projet.image)
+	return (
+		<div className="ProjectPreview">
+			<img src={importedImage} alt={projet.nom} />
+		</div>
+	)
 }
