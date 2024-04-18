958595390e80fa412fc82b14a1fbe098792a1367
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-18 20:46:37 +0200

    Started sorter logic + some style

--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -1,7 +1,30 @@
-//import '../assets/css/Sorter.css'
+import '../assets/css/Sorter.css'
+import { useSelector, useStore } from 'react-redux'
+import { selectionSlice } from '../slices/selectionSlice.js'
+import sortingPoints from '../assets/sortingPoints.json'
+import { getSelection } from '../app/selectors.js'
 
 export const Sorter = () => {
+	const store = useStore()
+	const selection = useSelector(getSelection)
 
+	const addToStore = (payload) => {
+		store.dispatch(selectionSlice.actions.addToSelection(payload))
+	}
 
-	return <div className='Sorter'>Sorter</div>
+	return (
+		<div className="Sorter">
+			{sortingPoints.map((item, index) => (
+				<button
+					key={index}
+					onClick={() => addToStore(item.name)}
+					className={`${
+						selection.includes(item.name) ? 'selected' : ''
+					}`}
+				>
+					{item.displayName}
+				</button>
+			))}
+		</div>
+	)
 }
