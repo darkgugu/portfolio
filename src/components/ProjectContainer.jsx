2b785355457120c2e6051d0775dae236f253331d
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-23 01:04:36 +0200

    Implemented project sort

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -2,16 +2,31 @@ import '../assets/css/ProjectContainer.css'
 import projects from '../assets/projets.json'
 import { ProjectPreview } from './ProjectPreview'
 import { Sorter } from './Sorter'
+import { useSelector } from 'react-redux'
+import { getSelection } from '../app/selectors.js'
 
 export const ProjectContainer = () => {
+	const selection = useSelector(getSelection)
+	let sortedProjects = projects
+	selection.forEach(
+		(tag) =>
+			(sortedProjects = sortedProjects.filter((projet) =>
+				projet.tags.includes(tag)
+			))
+	)
+
 	return (
 		<div className="ProjectContainer">
 			<h2>MES PROJETS</h2>
 			<Sorter />
 			<div className="projets">
-				{projects.map((projet, index) => (
-					<ProjectPreview projet={projet} key={index} />
-				))}
+				{selection.length !== 0
+					? sortedProjects.map((projet, index) => (
+							<ProjectPreview projet={projet} key={index} />
+					  ))
+					: projects.map((projet, index) => (
+							<ProjectPreview projet={projet} key={index} />
+					  ))}
 			</div>
 		</div>
 	)
