dee4a9225f44345cd4179e057bf427b865b11950
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-24 13:46:18 +0200

    Add openModale

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -4,8 +4,11 @@ import { ProjectPreview } from './ProjectPreview'
 import { Sorter } from './Sorter'
 import { useSelector } from 'react-redux'
 import { getSelection } from '../app/selectors.js'
+import { useState } from 'react'
+import { ProjectModal } from './ProjectModal.jsx'
 
 export const ProjectContainer = () => {
+	const [isModaleOpen, setIsModaleOpen] = useState(false)
 	const selection = useSelector(getSelection)
 	let sortedProjects = projects
 	selection.forEach(
@@ -15,19 +18,35 @@ export const ProjectContainer = () => {
 			))
 	)
 
+	const openModale = () => {
+		setIsModaleOpen(true)
+	}
+
 	return (
 		<div className="ProjectContainer">
 			<h2>MES PROJETS</h2>
 			<Sorter />
-			<div className="projets">
-				{selection.length !== 0
-					? sortedProjects.map((projet, index) => (
-							<ProjectPreview projet={projet} key={index} />
-					  ))
-					: projects.map((projet, index) => (
-							<ProjectPreview projet={projet} key={index} />
-					  ))}
-			</div>
+			{!isModaleOpen ? (
+				<div className="projets">
+					{selection.length !== 0
+						? sortedProjects.map((projet, index) => (
+								<ProjectPreview
+									projet={projet}
+									key={index}
+									onClick={() => openModale()}
+								/>
+						  ))
+						: projects.map((projet, index) => (
+								<ProjectPreview
+									projet={projet}
+									key={index}
+									onClick={() => openModale()}
+								/>
+						  ))}
+				</div>
+			) : (
+				<ProjectModal />
+			)}
 		</div>
 	)
 }
