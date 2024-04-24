cfa8bb7d6e3dc37c0a3126544cd30aacce11b6ea
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-24 13:46:10 +0200

    Beginning of hover mechanics

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
 import '../assets/css/ProjectPreview.css'
 import '../assets/sortingPoints.json'
 
-export const ProjectPreview = ({ projet }) => {
+export const ProjectPreview = ({ projet, onClick }) => {
+	const [isHover, setIsHover] = useState(false)
+	const start = () => {
+		//setIsHover(true)
+		//console.log('start')
+	}
+	const stop = () => {
+		//setIsHover(false)
+		//console.log('stop')
+	}
 	const importedImage = require('../assets/images/' + projet.image)
+	const importedVideo = require('../assets/videos/printit.webm')
 	return (
-		<div className="ProjectPreview">
-			<img src={importedImage} alt={projet.nom} />
+		<div
+			className="ProjectPreview"
+			onMouseEnter={() => start()}
+			onMouseLeave={() => stop()}
+			onClick={onClick}
+		>
+			{!isHover ? (
+				<img
+					className="preview-media"
+					src={importedImage}
+					alt={projet.nom}
+				/>
+			) : (
+				<video className="preview-media">
+					<source src={importedVideo} />
+				</video>
+			)}
 			<div className="content">
 				<p className="name">{projet.nom}</p>
 				<p className="desc">{projet.description}</p>
