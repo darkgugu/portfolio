7554a1b7d22a7cd3ee1902547f89e99a0039af4d
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-29 15:14:29 +0200

    Added meaningful content to Intro & Project Preview

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,15 +6,16 @@ export const Intro = () => {
 		<div className="Intro" id="accueil">
 			<img src={portrait} alt="Portrait Icham Duret" />
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-				fringilla enim mauris, at commodo purus sagittis et. Cras
-				faucibus nec quam eget tempus. Maecenas venenatis volutpat
-				metus. Proin eu dui dui. Interdum et malesuada fames ac ante
-				ipsum primis in faucibus. Ut scelerisque leo elit, ac vehicula
-				sapien vehicula nec. Sed maximus nulla orci, in tempor dui
-				consequat eu. In sit amet sodales tellus. Cras auctor odio a
-				magna venenatis eleifend. Morbi dapibus justo non interdum
-				rutrum. Proin nec interdum turpis, a ultricies elit.
+				<span className="title">
+					Bonjour ! Je suis <span className="name">Icham</span>,
+					développeur front-end
+				</span>
+				<br />
+				Diplômé d'OpenClassrooms en 2024, habitué du framework React, je
+				maitrise aussi bien le JavaScript que que le Sass ou
+				l'architecture Flux. En plus de mes projets Openclassrooms, j'ai
+				déjà réalisé des projets scolaires ou personnels en PHP,
+				Node.js, MySQL ou encore Java8
 			</p>
 		</div>
 	)
