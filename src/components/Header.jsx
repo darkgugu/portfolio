27e64db8563b44dbf89427c36543743d03a07ea8
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-29 14:19:04 +0200

    Add working CV button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
 import { useMediaQuery } from 'react-responsive'
 import '../assets/css/Header.css'
 import { Link } from 'react-router-dom'
+import cv from '../assets/CV_DURET_Icham.pdf'
 
 export const Header = () => {
 	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1025px)' })
 
 	const handleClick = () => {
-		//Download CV
+		// using Java Script method to get PDF file
+		fetch(cv).then((response) => {
+			response.blob().then((blob) => {
+				// Creating new object of PDF file
+				const fileURL = window.URL.createObjectURL(blob)
+
+				// Setting various property values
+				let alink = document.createElement('a')
+				alink.href = fileURL
+				alink.download = 'CV_DURET_Icham.pdf'
+				alink.click()
+			})
+		})
 	}
 	return (
 		<header className="Header">
