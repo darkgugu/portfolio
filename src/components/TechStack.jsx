246316b15194b46c73aa26d3da5b11302873af13
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-05-03 14:02:57 +0200

    Add blur effect and name to techStack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,16 +1,42 @@
+import { useState } from 'react'
 import '../assets/css/TechStack.css'
 import jsIcon from '../assets/images/technos/js.png'
 import reactIcon from '../assets/images/technos/react.png'
 import sassIcon from '../assets/images/technos/sass.png'
 
 export const TechStack = () => {
+	const [nameDisplayed, setNameDisplayed] = useState(false)
 	return (
 		<div className="TechStack" id="stack">
 			<h2>Stack Technique</h2>
 			<div className="technos">
-				<img src={jsIcon} alt="Logo javascript" />
-				<img src={reactIcon} alt="Logo React" />
-				<img src={sassIcon} alt="Logo Sass" />
+				<div className="img-span-container">
+					<img
+						onMouseEnter={() => setNameDisplayed('js')}
+						onMouseLeave={() => setNameDisplayed(false)}
+						src={jsIcon}
+						alt="Logo javascript"
+					/>
+					{nameDisplayed === 'js' ? <span>JavaScript</span> : null}
+				</div>
+				<div className="img-span-container">
+					<img
+						onMouseEnter={() => setNameDisplayed('react')}
+						onMouseLeave={() => setNameDisplayed(false)}
+						src={reactIcon}
+						alt="Logo react"
+					/>
+					{nameDisplayed === 'react' ? <span>React</span> : null}
+				</div>
+				<div className="img-span-container">
+					<img
+						onMouseEnter={() => setNameDisplayed('sass')}
+						onMouseLeave={() => setNameDisplayed(false)}
+						src={sassIcon}
+						alt="Logo sass"
+					/>
+					{nameDisplayed === 'sass' ? <span>Sass</span> : null}
+				</div>
 			</div>
 		</div>
 	)
